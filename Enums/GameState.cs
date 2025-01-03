84515642ba5cb487db56424cd81eef1fd7eaaf32
hoax <jumbo176176@gmail.com>
2025-01-03 13:49:18 +0200

    add enums lesson

---
+++ Enums/GameState.cs
@@ -0,0 +1,26 @@
+﻿namespace Enums
+{
+    public enum GameState
+    {
+        // Started = 0
+        Started,
+        // Running = 0
+        Running = Started,
+        // Если бы не было указано значение явно, то было бы 1 
+        Paused = 3,
+        // Нумерация зависит от предыдущего значения, поэтому Finished = 4
+        Finished
+    }
+
+
+    // Другой пример с использованием long, на случай если надо указать большие числа
+    // long можно заменить на любой другой целочисленный тип данных (sbyte, short, uint и тд)
+    // По умолчанию используется int
+    public enum GameState2 : long
+    {
+        Started = 1000,
+        Running = 1_000_000,
+        Paused = 1_000_000_000,
+        Finished = 1_000_000_000_000
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using Enums;
+
+namespace Lessons.Enums
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Простое использование enum
+            GameState gameState = GameState.Finished;
+
+            if (gameState == GameState.Finished)
+            {
+                Console.WriteLine($"GameState = {gameState}. Игра окончена");
+            }
+
+
+            // Приведение значения enum к числу
+            // Т.к. Finished = 4, то результат и будет 4
+            int gameStateNumber = (int)gameState;
+            Console.WriteLine($"Приведение {gameState} к числу, результат: {gameStateNumber}");
+
+
+            // Приведение числа к enum
+            // Результат: Paused
+            int number = 3;
+            gameState = (GameState)number;
+            Console.WriteLine($"Приведение числа {number} к GameState, результат: {gameState}");
+
+
+            // Приведение числа к несуществующему значению enum
+            // Результат: 111
+            number = 111;
+            gameState = (GameState)number;
+            Console.WriteLine($"Приведение числа {number} к несуществующему значению GameState, результат: {gameState}");
+        }
+    }
+}
